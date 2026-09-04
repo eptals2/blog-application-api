0b07442a583d6ee6dca40cbb2fb7969e49e206c7
Ephraim Serrano Talaba <eptals2@gmail.com>
2026-09-05 03:04:24 +0800

    Add CORS config and make posts index public

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/posts', [PostController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::get('/posts', [PostController::class, 'index']);
     Route::get('/posts/{id}', [PostController::class, 'show']);
 
     Route::middleware('admin')->group(function () {
